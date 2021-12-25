5e78a7a200c94ed937eca6825e89f98f39b88c18
Lorenzo Soto <lorenzo.soto@gmail.com>
2021-12-25 22:54:21 +0100

    Added update to latest firmware in web

--- web/src/components/Firmware.js
+++ web/src/components/Firmware.js
@@ -47,7 +47,24 @@ export default function Firmware() {
     setProgress(-1);
   }
 
+  async function updateToCurrent() {
+    setProgress(0);
+    try {
+      const fwDownload = await fetch(`/tocata-pedal/TocataPedal-${latestVersion}.uf2`)
+      const fwBlob = await fwDownload.blob()
+      const uf2 = new UF2(await fwBlob.arrayBuffer())
+      setProgress(25);
+      await flash(uf2, p => setProgress(p / 2));
+      await verify(uf2, p => setProgress(100 / 2 + p / 2));
+      await restart();
+    } catch (error) {
+      console.error(error);
+    }
+    setProgress(-1);
+  }
+
   const {major, minor, subminor} = version();
+  const latestVersion = process.env.REACT_APP_VERSION;
 
   return (
     <div>
@@ -56,7 +73,16 @@ export default function Firmware() {
         direction="column"
         alignItems="flex-start"
       >
-        <h3>Version: {major}.{minor}.{subminor}</h3>
+        <h3>Version: {major}.{minor}.{subminor} - Latest: {latestVersion}</h3>
+        <Button
+          color="primary"
+          variant="contained"
+          className={classes.root}
+          onClick={updateToCurrent}
+          disabled={progress >= 0}
+        >
+          Update to latest
+        </Button>
         <input
           accept=".uf2"
           id="restore-file"
@@ -72,7 +98,7 @@ export default function Firmware() {
             disabled={progress >= 0}
             className={classes.root}
           >
-            Firmware update
+            Update to file...
           </Button>
         </label>
         <Button
