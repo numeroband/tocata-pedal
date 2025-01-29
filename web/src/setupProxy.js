d36ec67aab24f811cb223a9c8f12093b4cc348a0
Lorenzo Soto <lorenzo.soto@gmail.com>
2025-01-29 13:16:39 +0100

    Fixed npm start

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://localhost:8080' }));
-  app.use(proxy('/ws', { target: 'ws://localhost:8080', ws: true }));
+  app.use(createProxyMiddleware('/api', { target: 'http://localhost:8080' }));
+  // app.use(createProxyMiddleware('/ws', { target: 'ws://localhost:8080', ws: true }));
 };
